ba6ab021e5497b040b03ed83be88393ecdd2da2a
Julien-Hashchak <2369740@cegepmontpetit.ca>
2025-11-23 20:55:43 -0500

    5

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,12 +1,27 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
     this.init();
+    
   }
 
   init() {
     Icons.load();
+    this.initSwiperPagination();
+  }
+
+  initSwiperPagination() {
+    const target = document.querySelector('.pagination');
+    if (!target) return;
+
+    new Swiper(target, {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+    });
   }
 }
 new Main();
